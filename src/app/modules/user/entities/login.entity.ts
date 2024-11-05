6df448c87de6941b1be1df19a728b32b19c9d72c
Filipe <filipeferreiramariz@outlook.com>
2024-11-05 14:17:29 -0300

    fix: correction at login

--- src/app/modules/user/entities/login.entity.ts
+++ src/app/modules/user/entities/login.entity.ts
@@ -3,6 +3,9 @@ import { ObjectType, Field, InputType } from '@nestjs/graphql';
 @InputType()
 export class LoginUser {
     @Field()
+    authType: string;
+
+    @Field()
     userId: string;
 
     @Field()
